ea269a0a431dd1f90a03a0145c3f2dc4b1fd18c7
Sohil Jain <sohiljn708@gmail.com>
2023-04-07 23:31:30 +0530

    GITB changes qualty branch

---
+++ src/file/Change2.java
@@ -0,0 +1,19 @@
+package file;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import org.codehaus.groovy.ast.stmt.ThrowStatement;
+import org.testng.annotations.IObjectFactoryAnnotation;
+
+public class Change2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("The value of A"+ ClassLoader.getSystemResource(null));
+		System.out.println("The value of B"+Time.valueOf("AMerica"));
+		System.out.println("the value of C" + Date.class);
+
+	}
+
+}
